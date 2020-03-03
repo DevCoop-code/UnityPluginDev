e2f91096b449ff8032e3d32744fed779eae0db08
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-03-03 12:52:37 +0900

    feat: Make basic Mac OSX Unity Plugin XCode Project

---
+++ PluginSource/RenderingPlugin/RenderingPlugin/RenderingPlugin.cpp
@@ -0,0 +1,15 @@
+//
+//  RenderingPlugin.cpp
+//  RenderingPlugin
+//
+//  Created by HanGyo Jeong on 2020/03/03.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Unity/IUnityGraphics.h"
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTimeFromUnity(float t)
+{
+    
+}
